a47d3108719aa40f71dc77357cc63aef40f56bef
badger <badger@badger.local>
2022-03-19 17:18:42 +0200

    Collecting onboarding data

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
-import { useState } from "react";
 import { UncontrolledOnboardingFlow } from "./UncontrolledOnboardingFlow";
 
 const StepOne = ({goToNext}) => (
 	<>
 		<h1>Step 1</h1>
-		<button onClick={goToNext}>Next</button>
+		<button onClick={() => goToNext({name: "John Doe"})}>Next</button>
 	</>
 )
 
 const StepTwo = ({goToNext}) => (
 	<>
 		<h1>Step 2</h1>
-		<button onClick={goToNext}>Next</button>
+		<button onClick={() => goToNext({age: 100})}>Next</button>
 	</>
 )
 
 const StepThree = ({goToNext}) => (
 	<>
 		<h1>Step 3</h1>
-		<button onClick={goToNext}>Next</button>
+		<button onClick={(() => goToNext({hairColor: 'brown'}))}>Next</button>
 	</>
 )
 
 function App() {
 	return (
-		<UncontrolledOnboardingFlow>
+		<UncontrolledOnboardingFlow onFinish={data => {
+			console.log(data)
+			alert('Onboarding complete')
+		}}>
 			<StepOne />
 			<StepTwo />
 			<StepThree />
