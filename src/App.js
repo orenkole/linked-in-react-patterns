6048860a995db5bf26f84b2b942385b51939f4e4
badger <badger@badger.local>
2022-03-19 17:59:23 +0200

    Controlled onboarding flows

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { UncontrolledOnboardingFlow } from "./UncontrolledOnboardingFlow";
+import { useState } from "react";
+import { ControlledOnboardingFlow } from "./ControlledOnboardingFlow";
 
 const StepOne = ({goToNext}) => (
 	<>
@@ -10,7 +11,7 @@ const StepOne = ({goToNext}) => (
 const StepTwo = ({goToNext}) => (
 	<>
 		<h1>Step 2</h1>
-		<button onClick={() => goToNext({age: 100})}>Next</button>
+		<button onClick={() => goToNext({age: 50})}>Next</button>
 	</>
 )
 
@@ -21,16 +22,32 @@ const StepThree = ({goToNext}) => (
 	</>
 )
 
+const StepFour = ({goToNext}) => (
+	<>
+		<h1>Step 4</h1>
+		<button onClick={(() => goToNext({hairColor: 'brown'}))}>Next</button>
+	</>
+)
+
 function App() {
+	const [onboardingData, setOnboardingData] = useState({});
+	const [currentIndex, setCurrentIndex] = useState(0);
+	
+	const onNext = (stepData) => {
+		setOnboardingData({...onboardingData, ...stepData});
+		setCurrentIndex(currentIndex + 1);
+	}
+
 	return (
-		<UncontrolledOnboardingFlow onFinish={data => {
-			console.log(data)
-			alert('Onboarding complete')
-		}}>
+		<ControlledOnboardingFlow 
+			currentIndex={currentIndex}
+			onNext={onNext}
+		>
 			<StepOne />
 			<StepTwo />
-			<StepThree />
-		</UncontrolledOnboardingFlow>
+			{onboardingData.age > 62 && <StepThree />}
+			<StepFour />
+		</ControlledOnboardingFlow>
 	)
 }
 
