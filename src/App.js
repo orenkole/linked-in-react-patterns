0f09b2af14845a25160a2fc16de007c7d797fe48
badger <badger@badger.local>
2022-03-19 13:11:50 +0200

    Uncontrolled onboarding flows

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import { useState } from "react";
-import { ControlledModal } from "./ControlledModal";
+import { UncontrolledOnboardingFlow } from "./UncontrolledOnboardingFlow";
+
+const StepOne = ({goToNext}) => (
+	<>
+		<h1>Step 1</h1>
+		<button onClick={goToNext}>Next</button>
+	</>
+)
+
+const StepTwo = ({goToNext}) => (
+	<>
+		<h1>Step 2</h1>
+		<button onClick={goToNext}>Next</button>
+	</>
+)
+
+const StepThree = ({goToNext}) => (
+	<>
+		<h1>Step 3</h1>
+		<button onClick={goToNext}>Next</button>
+	</>
+)
 
 function App() {
-	const [shouldShowModal, setShouldShowModal] = useState(false);
 	return (
-		<>
-			<ControlledModal
-				shouldShowModal={shouldShowModal}
-				onRequestClose={() => {setShouldShowModal(false);}}
-			>
-				<h1>Hello</h1>
-			</ControlledModal>
-			<button onClick={() => setShouldShowModal(!shouldShowModal)}>
-				{shouldShowModal ? 'Hide modal' : 'Show modal'}
-			</button>
-		</>
-	);
+		<UncontrolledOnboardingFlow>
+			<StepOne />
+			<StepTwo />
+			<StepThree />
+		</UncontrolledOnboardingFlow>
+	)
 }
 
 export default App;
